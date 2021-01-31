f156c5f4ec75474a0969fc530796f4c2ee30aed8
MasDenBy <horssupport@gmail.com>
2021-01-31 20:25:50 +0100

    Configure testing with Jest

--- api/tests/libraries/controllers/libraries.controller.test.ts
+++ api/tests/libraries/controllers/libraries.controller.test.ts
@@ -1,22 +1,13 @@
 import { app } from '../../../src/app';
 import supertest from "supertest";
 
-import { LibrariesController } from '../../../src/libraries/controllers/libraries.controller';
-import { LibraryService } from '../../../src/libraries/services/library.service';
-import { Config } from '../../../src/config';
-import { injectable } from 'inversify';
-import { container } from '../../../src/inversify.config';
-import debug, { log } from 'debug';
-
-const debugLog: debug.IDebugger = debug('test');
-
 describe('/libraries', () => {
 
     beforeEach(()=> {
 
     });
 
-    it('/', async () => {
+    test('/', async () => {
         const result = await supertest(app).get('/');
 
         expect(result.status).toBe(200);
