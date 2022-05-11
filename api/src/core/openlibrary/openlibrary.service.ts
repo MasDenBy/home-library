9df63b83bffc5dbf4bf21b73bfac5410c92fb8b3
MasDenBy <horssupport@gmail.com>
2022-05-11 19:23:38 +0200

    #89 log exceptions from openlibrary api

--- api/src/core/openlibrary/openlibrary.service.ts
+++ api/src/core/openlibrary/openlibrary.service.ts
@@ -1,33 +1,42 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import axios from 'axios';
 
 import { BookInfo, SearchResponseDto } from './openlibrary.dto';
 
 @Injectable()
 export class OpenLibraryService {
+  constructor(private readonly logger: Logger) { }
   private readonly serviceUrl: string = 'http://openlibrary.org';
 
   public async search(pattern: string): Promise<string> {
-    const response = await axios.get<SearchResponseDto>(
-      `${this.serviceUrl}/search.json?q=${OpenLibraryService.preparePattern(
-        pattern,
-      )}`,
-    );
-
-    if (response.data.numFound > 0 && response.data.docs[0].isbn?.length > 0) {
-      return response.data.docs[0].isbn[0];
+    try {
+      const response = await axios.get<SearchResponseDto>(
+        `${this.serviceUrl}/search.json?q=${OpenLibraryService.preparePattern(
+          pattern,
+        )}`,
+      );
+  
+      if (response.data.numFound > 0 && response.data.docs[0].isbn?.length > 0) {
+        return response.data.docs[0].isbn[0];
+      }
+    } catch (error) {
+      this.logger.error(error);
     }
 
     return null;
   }
 
   public async findByIsbn(isbn: string): Promise<BookInfo> {
-    const response = await axios.get(
-      `${this.serviceUrl}/api/books?bibkeys=isbn:${isbn}&jscmd=details&format=json`,
-    );
-
-    if (response.data) {
-      return response.data[`isbn:${isbn}`] as BookInfo;
+    try {
+      const response = await axios.get(
+        `${this.serviceUrl}/api/books?bibkeys=isbn:${isbn}&jscmd=details&format=json`,
+      );
+  
+      if (response.data) {
+        return response.data[`isbn:${isbn}`] as BookInfo;
+      }
+    } catch (error) {
+      this.logger.error(error);
     }
 
     return null;
